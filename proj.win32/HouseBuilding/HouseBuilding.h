d9e07112d3cd9694b6b981bc7a2c004f91f222d6
tankorsmash <tankorsmash@gmail.com>
2015-09-12 23:45:08 -0400

    indent

--- proj.win32/HouseBuilding/HouseBuilding.h
+++ proj.win32/HouseBuilding/HouseBuilding.h
@@ -12,84 +12,84 @@ typedef bool(*BoolFuncBuilding)(const Building*);
 
 class Updateable
 {
-public:
-    Clock* update_clock;
+    public:
+        Clock* update_clock;
 
-    Updateable();
-    void update(float dt);
+        Updateable();
+        void update(float dt);
 };
 
 class Nameable
 {
-public:
-    std::string name;
+    public:
+        std::string name;
 
-    Nameable(std::string name) :name(name) {};
+        Nameable(std::string name) :name(name) {};
 };
 
 class Building : public Nameable, public Updateable
 {
-public:
-    unsigned int num_workers; //people who work here, help make things faster
-    Building(std::string name, VoidFuncBuilding task) : task(task), Nameable(name), Updateable()
+    public:
+        unsigned int num_workers; //people who work here, help make things faster
+        Building(std::string name, VoidFuncBuilding task) : task(task), Nameable(name), Updateable()
     {
 
         num_workers = 1;
     };
 
-    void do_task()
-    {
-        if (this->task)
+        void do_task()
         {
-            this->task(this);
+            if (this->task)
+            {
+                this->task(this);
+            };
         };
-    };
 
-    VoidFuncBuilding task = NULL; //shop might sell product, farm creates ingredients, etc
+        VoidFuncBuilding task = NULL; //shop might sell product, farm creates ingredients, etc
 };
 
 class Village : public Nameable, public Updateable
 {
-public:
+    public:
 
-    std::vector<std::shared_ptr<Building>> buildings;
+        std::vector<std::shared_ptr<Building>> buildings;
 
-    Village(std::string name) : Nameable(name), Updateable()
+        Village(std::string name) : Nameable(name), Updateable()
     {
         buildings = {};
     };
 
 
-    void update(float dt);
+        void update(float dt);
 
 
-    void run_tasks_once()
-    {
-        for (std::shared_ptr<Building> building : this->buildings)
+        void run_tasks_once()
         {
-            building->do_task();
+            for (std::shared_ptr<Building> building : this->buildings)
+            {
+                building->do_task();
+            };
         };
-    };
 };
 
 
 class Animal : public Nameable
 {
-public:
-    Animal(std::string name) : Nameable(name) {};
+    public:
+        Animal(std::string name) : Nameable(name) {};
 };
 
 class Person : public Nameable
 {
-public:
-    Person(std::string name) : Nameable(name) {};
+    public:
+        Person(std::string name) : Nameable(name) {};
 };
 
 class Player : public Person
 {
-public:
-    unsigned int coins;
-    Player(std::string name) : Person(name) {};
+    public:
+        unsigned int coins;
+        Player(std::string name) : Person(name) {};
 };
 
 class Ingredient : public Nameable
