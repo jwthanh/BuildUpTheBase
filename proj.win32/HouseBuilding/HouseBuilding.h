c5f843487c44474b3a9aef752c70601b626d9c22
tankorsmash <tankorsmash@gmail.com>
2015-09-12 21:53:10 -0400

    moved some stuff to header

---
+++ proj.win32/HouseBuilding/HouseBuilding.h
@@ -0,0 +1,94 @@
+#pragma once
+#ifndef HOUSEBUILDING_H
+#define HOUSEBUILDING_H
+
+
+class Building;
+
+typedef void(*VoidFuncBuilding)(const Building*);
+typedef bool(*BoolFuncBuilding)(const Building*);
+
+class Nameable
+{
+    public:
+        std::string name;
+
+        Nameable(std::string name) :name(name) {};
+};
+
+class Building : public Nameable
+{
+    public:
+        unsigned int num_workers; //people who work here, help make things faster
+        Building(std::string name, VoidFuncBuilding task) : task(task), Nameable(name)
+    {
+
+        num_workers = 1;
+    };
+
+        void do_task() 
+        {
+            if (this->task)
+            {
+                this->task(this);
+            };
+        };
+
+        VoidFuncBuilding task = NULL; //shop might sell product, farm creates ingredients, etc
+};
+
+class Village : public Nameable
+{
+    public:
+        Village(std::string name) : Nameable(name)
+    {
+        buildings = {};
+    };
+
+        void run_tasks_once()
+        {
+            for (std::shared_ptr<Building> building : this->buildings)
+            {
+                building->do_task();
+            };
+        };
+
+        std::vector<std::shared_ptr<Building>> buildings;
+
+};
+
+
+class Animal : public Nameable
+{
+    public:
+        Animal(std::string name) : Nameable(name) {};
+};
+
+class Person : public Nameable
+{
+    public:
+        Person(std::string name) : Nameable(name) {};
+};
+
+class Player : public Person
+{
+    public:
+        unsigned int coins;
+        Player(std::string name) : Person(name) {};
+};
+
+class Ingredient : public Nameable
+{
+    Ingredient(std::string name) : Nameable(name) {};
+};
+
+class Product : public Nameable
+{
+    Product(std::string name) : Nameable(name) {};
+};
+
+class Waste : public Nameable
+{
+    Waste() : Nameable("Waste") {};
+};
+#endif
