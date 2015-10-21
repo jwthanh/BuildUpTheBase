a20c51b69e825447a8dbea7d388700493803338c
tankorsmash <tankorsmash@gmail.com>
2015-10-20 23:07:21 -0400

    add Worker skeleton class

---
+++ proj.win32/HouseBuilding/Worker.cpp
@@ -0,0 +1,1 @@
+#include "Worker.cpp"
---
+++ proj.win32/HouseBuilding/Worker.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef WORKER_H
+#define WORKER_H
+
+
+//
+//attaches to a building and builds recipes once certain conditions are hit
+//
+class Worker : public Nameable
+{
+    public:
+        spBuilding building;
+
+        Worker(std::string name) : Nameable(name) {
+
+        };
+};
+
+#endif
