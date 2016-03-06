54f39d7c5801593cc6bf29b4416d5b5a681d40a1
tankorsmash <tankorsmash@gmail.com>
2016-03-06 17:09:37 -0500

    add beatup menu button images to menu_item

--- Classes/NuMenu.cpp
+++ Classes/NuMenu.cpp
@@ -15,9 +15,15 @@ void NuMenu::menu_init()
     //setup menu items
     auto inst = cocos2d::CSLoader::getInstance();
     for (auto i : {1,2,3,4,5}) {
-        auto menu_item = inst->createNode("editor/buttons/menu_item.csb")->getChildByName("menu_item_btn");
+        cocos2d::ui::Button* menu_item = (cocos2d::ui::Button*)inst->createNode("editor/buttons/menu_item.csb")->getChildByName("menu_item_btn");
+        menu_item->loadTextures("main_UI_export_10_x4.png", "main_UI_export_10_x4_pressed.png", "main_UI_export_10_x4_disabled.png", cocos2d::ui::TextureResType::PLIST);
         menu_item->removeFromParent();
         scrollview->addChild(menu_item);
+
+        if (i == 1) { menu_item->setEnabled(false); }
+        if (i == 2) { menu_item->setBright(false); }
+        if (i == 3) { menu_item->setEnabled(true); }
+        if (i == 4) { menu_item->setBright(true); }
     };
 
     float extra_margins = scrollview->getChildrenCount()*(sy(15.0f)*sy(1.0f));
