06f56510217ff090601d709a7ab99c7d0adcf42d
tankorsmash <tankorsmash@gmail.com>
2016-03-06 16:18:01 -0500

    base constructor for numenu

--- Classes/NuMenu.cpp
+++ Classes/NuMenu.cpp
@@ -1,1 +1,12 @@
 #include "NuMenu.h"
+
+void NuMenu::menu_init()
+{
+    //setup title
+    //setup backbutton
+
+    //setup scrollable
+    auto scrollview = this->create_center_scrollview();
+
+    //setup menu items
+}
--- Classes/NuMenu.h
+++ Classes/NuMenu.h
@@ -12,11 +12,19 @@ class ShopItem;
 class Beatup;
 class Scrollable;
 
+class NuItem : public Buyable, public cocos2d::ui::Widget
+{
+    NuItem(std::string id_key) : Buyable(id_key) {
+
+    };
+};
+
 class NuMenu : public GameLayer
 {
     public:
-
         CREATE_FUNC(NuMenu);
+
+        void menu_init();
 
         Beatup* beatup;
         cocos2d::Scene* beatup_scene;
