ce5726473c73aa22a8769078508ebd84ac94e7aa
tankorsmash <tankorsmash@gmail.com>
2016-03-20 22:04:10 -0400

    f5 reload resources, backtick to reload game, add shop button

--- Classes/HarvestScene.cpp
+++ Classes/HarvestScene.cpp
@@ -2,12 +2,53 @@
 #include "Harvestable.h"
 
 #include "GameLogic.h"
+#include <cocos2d/cocos/editor-support/cocostudio/ActionTimeline/CSLoader.h>
+#include "NuMenu.h"
+#include "Beatup.h"
+#include "Util.h"
 
 USING_NS_CC;
 
 bool HarvestScene::init()
 {
     FUNC_INIT(HarvestScene);
+
+    auto inst = CSLoader::getInstance();
+    Node* harvest_scene_editor = inst->createNode("editor/scenes/harvest_scene.csb");
+
+    auto shop_button = ui::Button::create(
+            "shop_banner.png",
+            "shop_banner_hili.png",
+            "shop_banner.png",
+            ui::TextureResType::PLIST
+            );
+    auto text_shop = ui::ImageView::create(
+        "text_shop.png",
+        ui::TextureResType::PLIST
+        );
+    text_shop->setPosition(Vec2(28, 17));
+    shop_button->addChild(text_shop);
+    shop_button->setScale(4);
+    
+    Node* shop_pos_node = harvest_scene_editor->getChildByName("shop_pos");
+    // shop_pos_node->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    shop_button->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
+    shop_button->setPosition(shop_pos_node->getPosition());
+    
+
+    shop_button->addTouchEventListener([this](Ref*, ui::Widget::TouchEventType evt)
+    {
+        if (evt == ui::Widget::TouchEventType::ENDED) {
+            auto scene = Scene::create();
+            BuyBuildingsNuMenu* building_menu = BuyBuildingsNuMenu::create(GameLogic::getInstance()->beatup);
+            scene->addChild(building_menu);
+
+            auto director = Director::getInstance();
+            director->pushScene(scene);
+        };
+    });
+
+    this->addChild(shop_button);
 
     this->add_harvestable();
 
@@ -40,4 +81,13 @@ void HarvestScene::onKeyReleased(EventKeyboard::KeyCode keyCode, Event* event)
         this->quit(NULL);
         event->stopPropagation();
     }
+    else if (keyCode == EventKeyboard::KeyCode::KEY_F5)
+    {
+        GameLogic::getInstance()->beatup->reload_resources();
+    }
+    else if (keyCode == EventKeyboard::KeyCode::KEY_GRAVE)
+    {
+        Director::getInstance()->popScene();
+    }
 };
+
--- raw/buildup_editor/cocosstudio/scenes/harvest_scene.csd
+++ raw/buildup_editor/cocosstudio/scenes/harvest_scene.csd
@@ -5,6 +5,18 @@
       <Animation Duration="0" Speed="1.0000" />
       <ObjectData Name="Scene" Tag="239" ctype="GameNodeObjectData">
         <Size X="960.0000" Y="640.0000" />
+        <Children>
+          <AbstractNodeData Name="shop_pos" ActionTag="60628340" Tag="266" IconVisible="False" RightMargin="914.0000" TopMargin="83.8287" BottomMargin="510.1713" Scale9Width="46" Scale9Height="46" ctype="ImageViewObjectData">
+            <Size X="46.0000" Y="46.0000" />
+            <AnchorPoint />
+            <Position Y="510.1713" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition Y="0.7971" />
+            <PreSize X="0.0479" Y="0.0719" />
+            <FileData Type="Default" Path="Default/ImageFile.png" Plist="" />
+          </AbstractNodeData>
+        </Children>
       </ObjectData>
     </Content>
   </Content>
