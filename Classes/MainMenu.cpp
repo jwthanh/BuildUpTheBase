145ae531f9731b866086a32223c926fe1fa64d61
tankorsmash <tankorsmash@gmail.com>
2016-04-14 10:27:01 -0400

    transfer 10 grain from the inventory to the workshop
    this needs to get the right type of ingredient

--- Classes/MainMenu.cpp
+++ Classes/MainMenu.cpp
@@ -18,6 +18,7 @@
 #include "NuMenu.h"
 #include "GameLogic.h"
 #include "StaticData.h"
+#include "Animal.h"
 
 USING_NS_CC;
 
@@ -1165,6 +1166,12 @@ ui::Widget* InventoryMenu::create_detail_alert(spBuilding building, Ingredient::
         {
             vsIngredient& ingredients = this->building->ingredients;
             if (ingredients.empty()){ return; }
+            Animal animal("WorkshopWagon");
+            animal.b2b_transfer(
+                this->building,
+                BUILDUP->city->building_by_name("The Workshop"),
+                Resource::Ingredient,
+                10);
 
             CCLOG("not implemented");
 
