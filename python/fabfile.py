ad4daedee69648be939d73fbe827c1eee22abf40
tankorsmash <tankorsmash@gmail.com>
2016-04-24 04:11:36 -0400

    update fabfile to update server

--- python/fabfile.py
+++ python/fabfile.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd, env, put
 from fabric.contrib.console import confirm
 
-from fab_config import HOSTS, TEMPLATE_DIR
+from fab_config import HOSTS, TEMPLATE_DIR, DJANGO_SERVER_DIR, ROOT_SERVER_DIR
 
 env.hosts = HOSTS
 
@@ -17,3 +17,10 @@ def update_changelog():
     print "putting changelog on server"
     with cd(TEMPLATE_DIR):
         put("changelog.txt", "index.html")
+
+def update_server():
+    with cd(DJANGO_SERVER_DIR):
+        print "pulling on remote server"
+        run("git pull")
+        print "restarting job"
+        run("~/webapps/buildup_server/apache2/bin/restart")
