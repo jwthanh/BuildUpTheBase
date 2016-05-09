7f25fff0ed5d4c896137b4ec2cab975fb98c41b2
tankorsmash <tankorsmash@gmail.com>
2016-05-08 22:27:36 -0400

    scale down faster than up in storage animation

--- Classes/Harvestable.cpp
+++ Classes/Harvestable.cpp
@@ -121,9 +121,9 @@ void Harvestable::animate_touch_start(cocos2d::Touch* touch)
         Node* raw_ingredient_count = building_info_panel->getChildByName("ingredient_count");
         ui::Text* ingredient_count = dynamic_cast<ui::Text*>(raw_ingredient_count);
         auto tint = TintTo::create(0.2f, 255, 0, 0);
-        auto tint_rev = TintTo::create(0.2f, 255, 255, 255);
+        auto tint_rev = TintTo::create(0.1f, 255, 255, 255);
         auto scale = ScaleTo::create(0.2f, 1.15f);
-        auto scale_rev = ScaleTo::create(0.2f, 1.0f);
+        auto scale_rev = ScaleTo::create(0.1f, 1.0f);
         ingredient_count->runAction(Sequence::create(
             Spawn::createWithTwoActions(tint, scale),
             Spawn::createWithTwoActions(tint_rev, scale_rev),
