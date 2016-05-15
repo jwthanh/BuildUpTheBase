626f9c29e67840a0b5ab66635cf8f423324f952f
tankorsmash <tankorsmash@gmail.com>
2016-05-14 23:06:25 -0400

    getters and setters for Tech stuff

--- Classes/Technology.cpp
+++ Classes/Technology.cpp
@@ -4,4 +4,27 @@
 TechTree::TechTree()
     : technologies({})
 {
+
+};
+
+Technology::Technology()
+    : _been_unlocked(false),
+    _ing_requirements({})
+{
+
+};
+
+void Technology::set_been_unlocked(bool val)
+{
+    this->_been_unlocked = val;
+};
+
+bool Technology::get_been_unlocked()
+{
+    return this->_been_unlocked;
+};
+
+mistIngredient Technology::get_ingredient_requirements()
+{
+    return this->_ing_requirements;
 };
--- Classes/Technology.h
+++ Classes/Technology.h
@@ -4,6 +4,8 @@
 
 #include "constants.h"
 
+#include "Resources.h"
+
 
 ///Container and handler for the tech researched
 class TechTree
@@ -19,10 +21,15 @@ class Technology
 {
     private:
         bool _been_unlocked;
+        mistIngredient _ing_requirements;
 
     public:
+        Technology();
+
         bool get_been_unlocked();
         void set_been_unlocked(bool val);
+
+        mistIngredient get_ingredient_requirements();
 };
 
 #endif
