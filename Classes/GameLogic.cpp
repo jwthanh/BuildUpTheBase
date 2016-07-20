733c57b9cb93f1a9f392db95bef2ebbe9de21d7f
tankorsmash <tankorsmash@gmail.com>
2016-07-20 02:43:39 -0400

    add message when a building has reached capacity

--- Classes/GameLogic.cpp
+++ Classes/GameLogic.cpp
@@ -42,11 +42,24 @@ void GameLogic::post_load()
 
         res_count_t old_count = map_get(original_ingredients, ing_type, 0);
 
-        if (new_count- old_count > 0.0)
+        if (new_count - old_count > 0.0)
         {
-            ss << "+Gained " << beautify_double(new_count - old_count) << " " << Ingredient::type_to_string(ing_type) << std::endl;
+            ss << "+Gained " << beautify_double(new_count - old_count) << " " << Ingredient::type_to_string(ing_type);
+
+            for (spBuilding building : BUILDUP->city->buildings)
+            {
+                if (building->is_storage_full_of_ingredients(ing_type))
+                {
+                    ss << " (Upgrade " << building->name << " to fit more!)";
+                }
+
+            }
+
+            ss << std::endl;
         }
     }
+
+
 
     ss << "\nIt's been " << beautify_double(hours_since_last_login.count()) << " hours since last login";
     CCLOG(ss.str().c_str());
