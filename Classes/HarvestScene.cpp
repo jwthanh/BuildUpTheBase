187d4dfc811eaa255bc7d5a98bf3f8e220efefb1
tankorsmash <tankorsmash@gmail.com>
2016-10-02 19:58:40 -0400

    clean harvester loading bar code
    this is so the particle effects can get put in if the percentages are different

--- Classes/HarvestScene.cpp
+++ Classes/HarvestScene.cpp
@@ -228,14 +228,22 @@ void BaseScene::create_goal_loadingbar()
     harvester_progress_panel->setVisible(false);
     ui::LoadingBar* harvester_loading_bar = dynamic_cast<ui::LoadingBar*>(harvester_progress_panel->getChildByName("harvester_loadingbar"));
     auto update_harvester_loading_panel = [this, harvester_progress_panel, harvester_loading_bar](float dt) {
-        if (BUILDUP->get_target_building()->name != "The Dump") {
+        if (BUILDUP->get_target_building()->name != "The Dump")
+        {
             harvester_progress_panel->setVisible(false);
         }
-        else {
+        else
+        {
             harvester_progress_panel->setVisible(true);
             auto harvestable = dynamic_cast<Harvestable*>(this->getChildByName("harvestable"));
-            if (harvestable) {
-                harvester_loading_bar->setPercent(this->harvestable->get_click_ratio()*100.0);
+            if (harvestable)
+            {
+                auto orig_percent = harvester_loading_bar->getPercent();
+                auto new_percent = this->harvestable->get_click_ratio()*100.0;
+                if (orig_percent != new_percent)
+                {
+                    harvester_loading_bar->setPercent(new_percent);
+                }
             }
         };
     };
