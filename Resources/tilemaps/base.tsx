cecffc784d4d749a933d2a0d76ec29c7c04a372f
tankorsmash <tankorsmash@gmail.com>
2016-11-17 01:15:01 -0500

    add missing tilemaps

---
+++ Resources/tilemaps/base.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="base" tilewidth="134" tileheight="134" tilecount="135" columns="9">
+ <image source="base.png" trans="ffffff" width="1206" height="2010"/>
+</tileset>
