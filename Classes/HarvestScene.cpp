8672ceac49c6c566902438735a59d2db06d4fa1a
tankorsmash <tankorsmash@gmail.com>
2016-11-20 02:03:35 -0500

    use TextureResType instead of int

--- Classes/HarvestScene.cpp
+++ Classes/HarvestScene.cpp
@@ -463,23 +463,23 @@ void BaseScene::create_building_choicelist()
             auto target_building = BUILDUP->get_target_building();
 
             std::string tex_name;
-            int res_type;
+            ui::Widget::TextureResType res_type;
 
             if (target_building == building)
             {
                 tex_name = "crosshair_red.png";
-                res_type = (int)ui::Widget::TextureResType::PLIST;
+                res_type = ui::Widget::TextureResType::PLIST;
             }
             else
             {
                 tex_name = img_path;
-                res_type = (int)ui::Widget::TextureResType::PLIST;
+                res_type = ui::Widget::TextureResType::PLIST;
             }
 
             ResourceData r_data = building_image->getRenderFile();
             if (r_data.file != tex_name || r_data.type != (int)res_type)
             {
-                building_image->loadTexture(tex_name, (ui::Widget::TextureResType)res_type);
+                building_image->loadTexture(tex_name, res_type);
             }
 
             if (building->is_storage_full_of_ingredients(building->punched_sub_type))
@@ -532,15 +532,15 @@ void BaseScene::create_building_choicelist()
         auto target_building = BUILDUP->get_target_building();
 
         std::string tex_name;
-        int res_type;
+        ui::Widget::TextureResType res_type;
 
         tex_name = img_path;
-        res_type = (int)ui::Widget::TextureResType::PLIST;
+        res_type = ui::Widget::TextureResType::PLIST;
 
         ResourceData r_data = building_image->getRenderFile();
         if (r_data.file != tex_name || r_data.type != (int)res_type)
         {
-            building_image->loadTexture(tex_name, (ui::Widget::TextureResType)res_type);
+            building_image->loadTexture(tex_name, res_type);
         }
 
         try_set_node_color(panel, Color3B::WHITE);
