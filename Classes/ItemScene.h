effdfbed2f1c45fb228e38ec176af152b02c6dcb
tankorsmash <tankorsmash@gmail.com>
2016-12-06 22:41:09 -0500

    add base for ItemScene

---
+++ Classes/ItemScene.h
@@ -0,0 +1,11 @@
+#ifndef ITEMSCENE_H
+#define ITEMSCENE_H
+
+#include "GameLayer.h"
+
+class ItemScene : public GameLayer
+{
+	CREATE_FUNC(ItemScene);
+};
+
+#endif
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -57,6 +57,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/Logging.cpp \
                    ../../Classes/Miner.cpp \
                    ../../Classes/NodeBuilder.cpp \
+                   ../../Classes/ItemScene.cpp \
                    ../../Classes/magic_particles/MagicScene.cpp \
                    ../../Classes/magic_particles/MagicEmitter.cpp \
                    ../../Classes/magic_particles/mp_cocos.cpp \
