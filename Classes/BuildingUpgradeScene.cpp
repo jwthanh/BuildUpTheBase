192d3dbf9877a37e25b78472dad1e18c4077867c
tankorsmash <tankorsmash@gmail.com>
2017-03-12 16:49:04 -0400

    include editor elements in building scene

--- Classes/BuildingUpgradeScene.cpp
+++ Classes/BuildingUpgradeScene.cpp
@@ -3,12 +3,15 @@
 #include "constants.h"
 
 #include "HouseBuilding.h"
+#include "NodeBuilder.h"
+#include "MiscUI.h"
 
 #include "2d/CCScene.h"
 #include "base/CCEventListenerKeyboard.h"
 #include "base/CCEventDispatcher.h"
 #include "base/CCEventListenerTouch.h"
 #include "base/CCDirector.h"
+#include "ui/UIButton.h"
 
 BuildingUpgradeScene* BuildingUpgradeScene::create(spBuilding building)
 {
@@ -37,6 +40,15 @@ cocos2d::Scene* BuildingUpgradeScene::createScene(spBuilding building)
 bool BuildingUpgradeScene::init(spBuilding building)
 {
     this->building = building;
+
+    auto editor_scene = get_prebuilt_node_from_csb("editor/scenes/building_detail_scene.csb");
+    editor_scene->removeFromParent();
+    auto panel = editor_scene->getChildByName("panel");
+    panel->removeFromParent();
+    this->addChild(panel);
+
+    auto back_btn = dynamic_cast<cocos2d::ui::Button*>(panel->getChildByName("back_btn"));
+    prep_back_button(back_btn);
 
 #ifdef _WIN32
     FUNC_INIT_WIN32(BuildingUpgradeScene);
