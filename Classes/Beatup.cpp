2c63cf46df4cd14edd779dc4041ca81bdb2a2620
tankorsmash <tankorsmash@gmail.com>
2017-06-11 21:36:58 -0400

    increase the bank storage by 100x

--- Classes/Beatup.cpp
+++ Classes/Beatup.cpp
@@ -75,7 +75,8 @@ res_count_t Beatup::get_max_coin_storage()
     spBuilding&& marketplace = BUILDUP->city->building_by_type(BuildingTypes::TheMarketplace);
     res_count_t num_researched = map_get(marketplace->techtree->tech_map, TechSubType::RaiseWalletCap, 0.0);
 
-    return scale_number_flat_pow(100000.0L, num_researched, 11.3L);
+    static const res_count_t base_coin_storage = 10000000.0L;
+    return scale_number_flat_pow(base_coin_storage, num_researched, 11.3L);
 
 };
 
