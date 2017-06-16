638a43e39c682d78deaa8923e6cc32e6d93b235d
tankorsmash <tankorsmash@gmail.com>
2017-06-16 00:51:05 -0400

    use LayoutParameters for the welcomeback modal

--- Classes/main_loop/SimulateMainLoop.cpp
+++ Classes/main_loop/SimulateMainLoop.cpp
@@ -13,6 +13,8 @@
 #include "StaticData.h"
 #include "NodeBuilder.h"
 #include "MiscUI.h"
+#include "Modal.h"
+#include "ui/UIListView.h"
 
 bool SimulateMainLoop::is_simulating = false;
 
@@ -62,31 +64,41 @@ void SimulateMainLoop::generate_WIP_welcome_message()
 
     auto city_ingredients = BUILDUP->get_all_ingredients();
 
-    int width = 4;
+    auto welcome_back_modal = BaseModal::create();
+    welcome_back_modal->set_title("Welcome back!");
+    running_scene->addChild(welcome_back_modal);
+
+    int width = 3;
     float cell_width = 150.0f;
     float cell_height = 150.0f;
 
     float cell_margin = 5.0f;
 
-    cocos2d::Vec2 initial_pos = get_center_pos(-300, 200);
+    cocos2d::Vec2 initial_pos = cocos2d::Vec2::ZERO;
     float x = initial_pos.x;
     float y = initial_pos.y;
 
     int i = 0;
+    cocos2d::ui::Layout* layout = cocos2d::ui::Layout::create();
+    layout->setLayoutType(cocos2d::ui::Layout::Type::HORIZONTAL);
+    cocos2d::ui::ListView* body_scroll = welcome_back_modal->get_body_scroll();
+    layout->setContentSize({ cell_width*width+cell_margin*width, cell_height+cell_margin});
+    body_scroll->addChild(layout);
     for (auto ing_type : ing_types)
     {
-        auto panel = get_prebuilt_node_from_csb("editor/details/report_ing_detail.csb")->getChildByName("panel");
-        panel->removeFromParent();
-        panel->setPosition(x, y);
-        running_scene->addChild(panel);
+        cocos2d::ui::Layout* panel = dynamic_cast<cocos2d::ui::Layout*>(get_prebuilt_node_from_csb("editor/details/report_ing_detail.csb")->getChildByName("panel"));
 
-        x += cell_width + cell_margin;
+        panel->removeFromParent();
+        layout->addChild(panel);
+
         i++;
         if (i >= width)
         {
             i = 0;
-            x = initial_pos.x;
-            y -= cell_height + cell_margin;
+            layout = cocos2d::ui::Layout::create();
+            layout->setLayoutType(cocos2d::ui::Layout::Type::HORIZONTAL);
+            layout->setContentSize({ cell_width*width+cell_margin*width, cell_height+cell_margin});
+            body_scroll->addChild(layout);
         }
 
         IngredientData ingredient_data(ing_type);
